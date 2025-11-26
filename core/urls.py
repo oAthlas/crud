d54c0ef59640332cbe45acbab0c071080b6b3203
Athlas <elderluiz72@gmail.com>
2025-11-25 22:59:59 -0300

    crud finished

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('create_users/', views.create_user, name='create_user'),
+    path('', views.create_user, name='create_user'),
     path('delete_user/<int:id>/', views.delete_user, name='delete_user'),
     path('update_user/<int:id>/', views.update_user, name='update_user'),
 ]
