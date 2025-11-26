b23f36e8038785612a27c04f60e8d25d5b8f9446
Athlas <elderluiz72@gmail.com>
2025-11-25 22:50:39 -0300

    first commit

---
+++ core/urls.py
@@ -0,0 +1,8 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('create_users/', views.create_user, name='create_user'),
+    path('delete_user/<int:id>/', views.delete_user, name='delete_user'),
+    path('update_user/<int:id>/', views.update_user, name='update_user'),
+]
---
+++ core/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import usuario
+from django.http import HttpResponse
+
+def create_user(request):
+    if request.method =='GET':
+        usuários = usuario.objects.all()
+        return render(request, 'create_user.html' , {'usuarios': usuários})
+    
+    elif request.method == 'POST':
+        name = request.POST.get('nome')
+        email = request.POST.get('email')
+
+        user = usuario(
+            name=name,
+            email=email
+        )
+
+        user.save()
+
+        return redirect('create_user')
+    
+def delete_user(request, id):
+    ususário = get_object_or_404(usuario, id=id)
+    ususário.delete()
+    return redirect('create_user')
+
+def update_user(request, id):
+    ususário = get_object_or_404(usuario, id=id)
+
+    name = request.POST.get('nome')
+    email = request.POST.get('email')
+    
+    ususário.name = name
+    ususário.email = email
+    ususário.save()
+
+    return redirect('create_user')
+            
